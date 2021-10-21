58703c73a3710a0546639be5f396682a27ccef53
Baibhav <baibhav030501@gmail.com>
2021-10-22 00:02:55 +0530

    public

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log('Client side JavaScript is LOADED !!!');
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
+    fetch('http://localhost:3000/weather?city=' + location).then((response) => {
+        response.json().then((data) => {
+            // console.log(data);
+            // console.log(data.forecast);
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                const forecast = data.forecast
+                messageOne.textContent = data.location
+                messageTwo.textContent = 'Temperature: ' + forecast.temperature
+                messageThree.textContent = 'Feels Like: ' + forecast.feels_like
+                // console.log(data.location);
+                // console.log(data.forecast);
+            }
+        })
+    })
+})
